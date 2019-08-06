b2e62eeb00187293e987547666a1afc6126e52d3
Aniket Satarkar <aniket.satarkar@neosofttech.com>
2019-08-06 17:31:54 +0530

    Added bootstrap

--- src/App.vue
+++ src/App.vue
@@ -1,31 +1,37 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
+    <!-- navbar -->
+    <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+      <a class="navbar-brand" href="/">
+        <img src="@/assets/logo.png" width="25" alt="todo" />
+      </a>
+      <button
+        class="navbar-toggler"
+        type="button"
+        data-toggle="collapse"
+        data-target="#navbarSupportedContent"
+        aria-controls="navbarSupportedContent"
+        aria-expanded="false"
+        aria-label="Toggle navigation"
+      >
+        <span class="navbar-toggler-icon"></span>
+      </button>
+
+      <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul class="navbar-nav mr-auto">
+          <li class="nav-item active">
+            <router-link class="nav-link" to="/">Home</router-link>
+          </li>
+          <li class="nav-item">
+            <router-link class="nav-link" to="/about">Todo</router-link>
+          </li>
+        </ul>
+      </div>
+    </nav>
+
+    <!-- routing outlet -->
+    <div class="container mt-5">
+      <router-view />
     </div>
-    <router-view />
   </div>
 </template>
-
-<style>
-#app {
-  font-family: "Avenir", Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-#nav {
-  padding: 30px;
-}
-
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-#nav a.router-link-exact-active {
-  color: #42b983;
-}
-</style>
---
+++ src/components/LinksHolder.vue
@@ -0,0 +1,18 @@
+<template>
+    <div>
+        <h5>Addtional links</h5>
+        <div class="link-container">
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "LinksHolder",
+        data(){
+            return{
+                links: [{url: '/sport',name : 'Sport News ',},{url: '/economy',name: 'Economy News '},{url: '/latest',name : 'Latest News '}],
+            }
+        }
+    }
+</script>
